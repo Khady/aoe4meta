5818ad3090dc1e23a3583195e6d635c514b22b3d
Louis Roché <louis@louisroche.net>
2025-10-01 02:30:45 +0800

    astro: setup site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://khady.github.io",
+  base: "/aoe4meta",
+  integrations: [react()],
 });
